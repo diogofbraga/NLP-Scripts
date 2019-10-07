2f62dd8c6b9257a169796a2d5a68d745afe4a05b
Diogo Braga <diogobraga@Air-de-Diogo.home>
2019-10-07 23:41:34 +0100

    check if a string contains a WHOLE word

--- TP1/nlgrep.py
+++ TP1/nlgrep.py
@@ -1,6 +1,13 @@
 import re
 import sys
 
+# check if a string contains a whole word
+def string_found(pat, sen):
+    # escape: Return string with all non-alphanumerics backslashed
+    if re.search(r"\b" + re.escape(pat) + r"\b", sen):
+        return True
+    return False
+
 # get arguments through command line
 flag = sys.argv[1]
 pattern = sys.argv[2]
@@ -17,8 +24,7 @@ if flag == '-s':
     sentences = re.split(r"(?<=[.?!\n])\s(?=[A-Z])", file)
     # for each sentence, search the pattern. If there are any positive results, the sentence is printed to the ouput.
     for s in sentences:
-        res = re.search(pattern, s)
-        if res is not None:
+        if (string_found(pattern,s)) is True:
             # remove \n's if there are any in the middle of the sentences
             print(s.replace('\n', ' '))
 # check if flag is paragraph-level
@@ -26,7 +32,6 @@ elif flag == '-p':
     paragraphs = re.split(r"\n\n", file)
     # for each paragraph, search the pattern. If there are any positive results, the paragraph is printed to the ouput.
     for p in paragraphs:
-        res = re.search(pattern, p)
-        if res is not None:
+        if (string_found(pattern,p)) is True:
             # remove \n's if there are any in the middle of the paragraphs
             print(p.replace('\n', ' '))
