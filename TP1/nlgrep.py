c05cbd754c0ce9e74019e38a9a8cedab938414dd
Diogo Braga <diogofiliperfbraga@gmail.com>
2019-10-08 00:16:44 +0100

    getting multiple files from command line

--- TP1/nlgrep.py
+++ TP1/nlgrep.py
@@ -8,18 +8,7 @@ def string_found(pat, sen):
         return True
     return False
 
-# get arguments through command line
-flag = sys.argv[1]
-pattern = sys.argv[2]
-# if there are 4 args (script name included), read the file given as argument
-if len(sys.argv) == 4:
-    file = open(sys.argv[3], 'r').read()
-# else get it from stdin
-else:
-    file = sys.stdin.readlines()
-    file = ''.join(file)
-# check if flag is sentence-level
-if flag == '-s':
+def pattern_sentence(file):
     # if a whitespace is preceded by .,!, ? or newline, it splits the file on the space.
     sentences = re.split(r"(?<=[.?!\n])\s(?=[A-Z])", file)
     # for each sentence, search the pattern. If there are any positive results, the sentence is printed to the ouput.
@@ -27,11 +16,36 @@ if flag == '-s':
         if (string_found(pattern,s)) is True:
             # remove \n's if there are any in the middle of the sentences
             print(s.replace('\n', ' '))
-# check if flag is paragraph-level
-elif flag == '-p':
+
+def pattern_paragraph(file):
     paragraphs = re.split(r"\n\n", file)
     # for each paragraph, search the pattern. If there are any positive results, the paragraph is printed to the ouput.
     for p in paragraphs:
         if (string_found(pattern,p)) is True:
             # remove \n's if there are any in the middle of the paragraphs
             print(p.replace('\n', ' '))
+
+# get arguments through command line
+flag = sys.argv[1]
+pattern = sys.argv[2]
+# if there are 4 args (script name included), read the file given as argument
+if len(sys.argv) == 3:
+    file = sys.stdin.readlines()
+    file = ''.join(file)
+# else get it from stdin
+else:
+    files = []
+    for i in range(3, len(sys.argv)):
+        files.append(sys.argv[i])
+
+# check if flag is sentence-level
+if flag == '-s':
+    for x in files:
+        file = open(x, 'r').read()
+        pattern_sentence(file)
+
+# check if flag is paragraph-level
+elif flag == '-p':
+    for x in files:
+        file = open(x, 'r').read()
+        pattern_paragraph(file)
